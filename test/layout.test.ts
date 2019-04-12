e8ca2bd4ff3aee692eed2b12ff15de2d0429f064
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-12 20:27:28 +0900

    test(layout): edgesizes

--- test/layout.test.ts
+++ test/layout.test.ts
@@ -1,5 +1,9 @@
-import { Rect } from "../src/layout";
+import { EdgeSizes, Rect } from "../src/layout";
 
 test("rect", () => {
   expect(new Rect(1, 2, 3, 4).height).toBe(4);
+});
+
+test("edge sizes", () => {
+  expect(new EdgeSizes(1, 2, 3, 4).bottom).toBe(4);
 });
