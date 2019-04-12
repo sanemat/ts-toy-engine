4412402d4315d2c466287d0150c540d4aa4bac76
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-12 20:18:48 +0900

    feat(layout): edge sizes

--- src/layout.ts
+++ src/layout.ts
@@ -11,3 +11,17 @@ export class Rect {
     this.height = height;
   }
 }
+
+export class EdgeSizes {
+  left: number;
+  right: number;
+  top: number;
+  bottom: number;
+
+  constructor(left: number, right: number, top: number, bottom: number) {
+    this.left = left;
+    this.right = right;
+    this.top = top;
+    this.bottom = bottom;
+  }
+}
