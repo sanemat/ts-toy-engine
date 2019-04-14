ba4abf3ec7bca70657b2223b032cab6fc36815e0
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-15 00:19:54 +0900

    test(layout): dimensions

--- test/layout.test.ts
+++ test/layout.test.ts
@@ -1,4 +1,4 @@
-import { EdgeSizes, Rect } from "../src/layout";
+import { Dimensions, EdgeSizes, Rect } from "../src/layout";
 
 test("rect", () => {
   expect(new Rect(1, 2, 3, 4).height).toBe(4);
@@ -6,4 +6,10 @@ test("rect", () => {
 
 test("edge sizes", () => {
   expect(new EdgeSizes(1, 2, 3, 4).bottom).toBe(4);
+});
+
+test("dimensions", () => {
+  const target = new Rect(1, 2, 3, 4);
+  const edge = new EdgeSizes(1, 2, 3, 4);
+  expect(new Dimensions(target, edge, edge, edge).content).toEqual(target);
 });
