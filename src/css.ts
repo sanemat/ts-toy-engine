2e31b55f95c601011b7ce41ad31dda524faabd94
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-20 17:32:15 +0700

    feat(css): value, pattern match

--- src/css.ts
+++ src/css.ts
@@ -11,3 +11,41 @@ export class Color {
     this.a = a;
   }
 }
+
+export enum Unit {
+  Px
+}
+
+export enum ValueFormat {
+  Keyword,
+  Length,
+  ColorValue
+}
+
+export class Keyword {
+  readonly format = ValueFormat.Keyword;
+  keyword: string;
+  constructor(keyword: string) {
+    this.keyword = keyword;
+  }
+}
+
+export class Length {
+  readonly format = ValueFormat.Length;
+  length: number;
+  unit: Unit;
+  constructor(length: number, unit: Unit) {
+    this.length = length;
+    this.unit = unit;
+  }
+}
+
+export class ColorValue {
+  readonly format = ValueFormat.ColorValue;
+  colorValue: Color;
+  constructor(colorValue: Color) {
+    this.colorValue = colorValue;
+  }
+}
+
+export type Value = Keyword | Length | ColorValue;
