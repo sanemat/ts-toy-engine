ee0a5a945de5fdacfbee2d649a3f7125598c6a31
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-21 14:44:00 +0700

    feat(painting): display command

--- src/painting.ts
+++ src/painting.ts
@@ -1,4 +1,5 @@
 import { Color } from "./css";
+import { Rect } from "./layout";
 
 export class Canvas {
   pixels: Array<Color>;
@@ -16,3 +17,20 @@ export class Canvas {
     return new Canvas(new Array(width * height).fill(white), width, height);
   }
 }
+
+export enum DisplayCommandFormat {
+  SolidColor
+}
+
+export class SolidColor {
+  readonly format = DisplayCommandFormat.SolidColor;
+  color: Color;
+  rect: Rect;
+  constructor(color: Color, rect: Rect) {
+    this.color = color;
+    this.rect = rect;
+  }
+}
+
+export type DisplayCommand = SolidColor;
+export type DisplayList = Array<DisplayCommand>;
--- test/painting.test.ts
+++ test/painting.test.ts
@@ -1,14 +1,21 @@
-import { Canvas } from "../src/painting";
+import { Canvas, DisplayCommandFormat, SolidColor } from "../src/painting";
 import { Color } from "../src/css";
+import { Rect } from "../src/layout";
 
 test("canvas pixels length", () => {
   const canvas = Canvas.Create(2, 3);
   expect(canvas.pixels.length).toEqual(6);
 });
 
+const white = new Color(255, 255, 255, 255);
 test("canvas is filled by white", () => {
   const canvas = Canvas.Create(2, 3);
-  const white = new Color(255, 255, 255, 255);
 
   expect(canvas.pixels).toEqual([white, white, white, white, white, white]);
+});
+
+test("solid color", () => {
+  expect(new SolidColor(white, new Rect(0, 0, 0, 0)).format).toEqual(
+    DisplayCommandFormat.SolidColor
+  );
 });
