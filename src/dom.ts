a31c4e8c406092ef0e060d0965e56253f6af7bc2
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-26 02:11:37 +0700

    wip

--- src/dom.ts
+++ src/dom.ts
@@ -1,1 +1,34 @@
 export class DomNode {}
+export type AttrMap = Map<string, string>;
+
+export namespace NodeType {
+  export enum Format {
+    Element,
+    Text
+  }
+
+  export class Element {
+    readonly format = Format.Element;
+    element: ElementData;
+    constructor(element: ElementData) {
+      this.element = element;
+    }
+  }
+
+  export class ElementData {
+    tagName: string;
+    attributes: AttrMap;
+    constructor(tagName: string, attributes: AttrMap) {
+      this.tagName = tagName;
+      this.attributes = attributes;
+    }
+  }
+
+  export class Text {
+    readonly format = Format.Text;
+    text: string;
+    constructor(text: string) {
+      this.text = text;
+    }
+  }
+}
