3fbaba32ab1ad4c9d03bb0fc1accc934a4eaeb0c
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-26 08:35:10 +0700

    wip

--- test/painting.test.ts
+++ test/painting.test.ts
@@ -1,5 +1,5 @@
 import { Canvas, DisplayCommand, getColor } from "../src/painting";
-import { Color } from "../src/css";
+import { Color, CssValue } from "../src/css";
 import { BoxType, LayoutBox, Rect } from "../src/layout";
 import { StyledNode } from "../src/style";
 import { DomNode, NodeType } from "../src/dom";
@@ -28,6 +28,24 @@ test("canvas paint item", () => {
   const canvas = Canvas.Create(2, 3);
   canvas.paintItem(new DisplayCommand.SolidColor(black, new Rect(0, 0, 0, 0)));
   expect(canvas.pixels).toEqual([black, white, white, white, white, white]);
+});
+
+test("get color", () => {
+  const expectedColor = new Color(255, 255, 255, 255);
+  expect(
+    getColor(
+      LayoutBox.Create(
+        new BlockNode(
+          new StyledNode(
+            new DomNode([], new NodeType.Text("text")),
+            new Map([["example", new CssValue.ColorValue(expectedColor)]]),
+            []
+          )
+        )
+      ),
+      "example"
+    )
+  ).toEqual(expectedColor);
 });
 
 test("get no color", () => {
