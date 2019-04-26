ec646d76ad50782b78fe374d1799449a0af1570a
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-26 07:26:58 +0700

    wip

--- test/style.test.ts
+++ test/style.test.ts
@@ -1,9 +1,13 @@
 import { StyledNode } from "../src/style";
 import { CssValue, Unit } from "../src/css";
-import { DomNode } from "../src/dom";
+import { DomNode, NodeType } from "../src/dom";
 
 test("styled node", () => {
   expect(
-    new StyledNode(new DomNode(), new Map([["1", new CssValue.Length(1, Unit.Px)]]), []).children
+    new StyledNode(
+      new DomNode([], new NodeType.Text("example")),
+      new Map([["1", new CssValue.Length(1, Unit.Px)]]),
+      []
+    ).children
   ).toEqual([]);
 });
