a49186f940a8f048a56f98ba5ce67769ef10c638
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-26 08:16:44 +0700

    wip

--- test/dom.test.ts
+++ test/dom.test.ts
@@ -2,3 +2,17 @@ import { DomNode, NodeType } from "../src/dom";
 test("node", () => {
   expect(() => new DomNode([], new NodeType.Text("example"))).not.toThrow();
 });
+
+test("element data", () => {
+  expect(() => new NodeType.ElementData("example", new Map([["key", "value"]]))).not.toThrow();
+});
+
+test("text", () => {
+  expect(() => new NodeType.Text("example")).not.toThrow();
+});
+
+test("element", () => {
+  expect(
+    () => new NodeType.Element(new NodeType.ElementData("example", new Map([["key", "value"]])))
+  ).not.toThrow();
+});
