43cae4867d264e314b65b52e6e71510bacffc9ca
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-27 15:57:24 +0700

    feat(style): StyledNode.value

--- src/style.ts
+++ src/style.ts
@@ -7,9 +7,14 @@ export class StyledNode {
   node: DomNode;
   specifiedValues: PropertyMap;
   children: StyledNode[];
+
   constructor(node: DomNode, specifiedValues: PropertyMap, children: StyledNode[]) {
     this.node = node;
     this.specifiedValues = specifiedValues;
     this.children = children;
+  }
+
+  value(name: string): CssValue | null {
+    return this.specifiedValues.get(name) || null;
   }
 }
--- test/style.test.ts
+++ test/style.test.ts
@@ -7,3 +7,18 @@ test("styled node", () => {
     new StyledNode(new DomNode(), new Map([["1", new CssValue.Length(1, Unit.Px)]]), []).children
   ).toEqual([]);
 });
+
+test("styled node hits", () => {
+  const expected = new CssValue.Keyword("example");
+  expect(
+    new StyledNode(new DomNode(), new Map([["target", expected]]), []).value("target")
+  ).toEqual(expected);
+});
+
+test("styled node does not hit", () => {
+  expect(
+    new StyledNode(new DomNode(), new Map([["some", new CssValue.Keyword("example")]]), []).value(
+      "different"
+    )
+  ).toEqual(null);
+});
