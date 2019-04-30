5627f0297da7afef9ebfe727513c2818fc06af27
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-30 17:12:59 +0700

    test(dom): check constructor

--- test/dom.test.ts
+++ test/dom.test.ts
@@ -1,4 +1,12 @@
-import { DomNode, NodeType } from "../src/dom";
+import { DomNode, ElementData, NodeType } from "../src/dom";
 test("node", () => {
   expect(() => new DomNode([], new NodeType.Text("no mean"))).not.toThrow();
+});
+
+test("node type text", () => {
+  expect(() => new NodeType.Text("no mean")).not.toThrow();
+});
+
+test("node type element", () => {
+  expect(() => new NodeType.Element(new ElementData("no mean", new Map([])))).not.toThrow();
 });
