9fad2aac881e8797b630d6f65c979b54a955ec79
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-30 17:21:52 +0700

    chore(example): dom shortcut

--- example/dom-shortcut.ts
+++ example/dom-shortcut.ts
@@ -1,6 +1,23 @@
+// $ node_modules/.bin/ts-node example/dom-shortcut.ts
+// DomNode {
+//   children: [ DomNode { children: [Array], nodeType: [Element] } ],
+//     nodeType: Element {
+//     format: 0,
+//       element: ElementData { tagName: 'html', attributes: Map {} }
+//   }
+// }
+//
 // <html><body>Hello, world!</body></html>
+
+import { DomNode, ElementData, NodeType } from "../src/dom";
 
 // let root = element("html");
 // let body = element("body");
 // root.children.push(body);
 // body.children.push(text("Hello, world!"));
+
+const root = new DomNode([], new NodeType.Element(new ElementData("html", new Map([]))));
+const body = new DomNode([], new NodeType.Element(new ElementData("body", new Map([]))));
+root.children.push(body);
+body.children.push(new DomNode([], new NodeType.Text("Hello, world!")));
+console.log(root);
