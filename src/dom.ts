f7b192732d8f2ea727cb837d632623101cc8cfac
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-30 16:50:47 +0700

    feat(dom): introduce dom

--- src/dom.ts
+++ src/dom.ts
@@ -1,1 +1,48 @@
-export class DomNode {}
+export class DomNode {
+  children: DomNode[];
+  nodeType: NodeType;
+
+  constructor(children: DomNode[], nodeType: NodeType) {
+    this.children = children;
+    this.nodeType = nodeType;
+  }
+}
+
+export namespace NodeType {
+  export enum Format {
+    Element,
+    Text
+  }
+
+  export class Element {
+    readonly format = Format.Element;
+    element: ElementData;
+
+    constructor(element: ElementData) {
+      this.element = element;
+    }
+  }
+
+  export class Text {
+    readonly format = Format.Text;
+    text: string;
+
+    constructor(text: string) {
+      this.text = text;
+    }
+  }
+}
+
+export type NodeType = NodeType.Element | NodeType.Text;
+
+type AttrMap = Map<string, string>;
+
+export class ElementData {
+  tagName: string;
+  attributes: AttrMap;
+
+  constructor(tagName: string, attributes: AttrMap) {
+    this.tagName = tagName;
+    this.attributes = attributes;
+  }
+}
