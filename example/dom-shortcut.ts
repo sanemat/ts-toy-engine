ccc021413df6b77d09318b2e7df041235dcb697e
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-04-30 16:23:01 +0700

    chore(example): dom shortcut

---
+++ example/dom-shortcut.ts
@@ -0,0 +1,6 @@
+// <html><body>Hello, world!</body></html>
+
+// let root = element("html");
+// let body = element("body");
+// root.children.push(body);
+// body.children.push(text("Hello, world!"));
