0423575896f55d444cc26f2241a578956be0b556
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-05-01 17:39:22 +0700

    chore(example): show children

--- example/dom-shortcut.ts
+++ example/dom-shortcut.ts
@@ -1,16 +1,8 @@
 // $ node_modules/.bin/ts-node example/dom-shortcut.ts
-// DomNode {
-//   children: [ DomNode { children: [Array], nodeType: [Element] } ],
-//     nodeType: Element {
-//     format: 0,
-//       element: ElementData { tagName: 'html', attributes: Map {} }
-//   }
-// }
-//
-// <html><body>Hello, world!</body></html>
-
 import { elem, text } from "../src/dom";
 
+// <html><body>Hello, world!</body></html>
+//
 // let root = element("html");
 // let body = element("body");
 // root.children.push(body);
@@ -20,4 +12,26 @@ const root = elem("html", new Map([]), []);
 const body = elem("body", new Map([]), []);
 root.children.push(body);
 body.children.push(text("Hello, world!"));
-console.log(root);
+console.dir(root, { depth: null });
+
+// DomNode {
+//   children: [
+//     DomNode {
+//       children: [
+//         DomNode {
+//           children: [],
+//           nodeType: Text { format: 1, text: 'Hello, world!' }
+//     }
+//   ],
+//     nodeType: Element {
+//     format: 0,
+//       element: ElementData { tagName: 'body', attributes: Map {} }
+//   }
+// }
+// ],
+//   nodeType: Element {
+//     format: 0,
+//       element: ElementData { tagName: 'html', attributes: Map {} }
+//   }
+// }
+//
