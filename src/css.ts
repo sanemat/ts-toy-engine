8813c6c5e77528071e2abcf064b94254e51ad01d
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-05-11 13:03:10 +0700

    feat(css): SimpleSelector#specificity

--- src/css.ts
+++ src/css.ts
@@ -79,6 +79,8 @@ export namespace Selector {
 
 export type Selector = Selector.Simple;
 
+export type Specificity = [number, number, number];
+
 export class SimpleSelector {
   tagName: string | null;
   id: string | null;
@@ -88,6 +90,15 @@ export class SimpleSelector {
     this.tagName = tagName;
     this.id = id;
     this.classValue = classValue;
+  }
+
+  specificity(): Specificity {
+    // http://www.w3.org/TR/selectors/#specificity
+    const a = this.id === null ? 0 : 1;
+    const b = this.classValue.length;
+    // NOTE: I don't support chained selectors now.
+    const c = this.tagName === null ? 0 : 1;
+    return [a, b, c];
   }
 }
 
